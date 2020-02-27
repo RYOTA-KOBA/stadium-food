fa060d8371db9c9ccad65563ec77472c8991c357
RYOTA <kobaryo04ysh@icloud.com>
2020-02-28 00:28:41 +0900

    deviseのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
-   }
 
  root 'posts#index'
+
+ devise_for :users, :controllers => {
+  :registrations => 'users/registrations',
+  :sessions => 'users/sessions'
+ }
+
+ devise_scope :user do
+  get 'users/:id/edit' => 'users/registrations#edit', as: :user_edit
+  patch 'users/:id' => 'users/registrations#update'
+  put 'users/:id' => 'users/registrations#update'
+ end
+
  resources :posts
  resources :comments, only: %i[create destroy]
  resources :testsessions, only: :create
- resources :users, only: %i[edit show update]
+ resources :users, only: %i[show]
 end
 
