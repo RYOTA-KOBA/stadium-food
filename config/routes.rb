c339bc9a9113d0dedb7153a2b6aa717ac2a37dcc
RYOTA <kobaryo04ysh@icloud.com>
2020-03-01 22:54:00 +0900

    ゲストユーザーログインの実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
  }
 
  devise_scope :user do
+  get "sign_in", :to => 'users/sessions#new'
   get 'users/:id/edit' => 'users/registrations#edit', as: :user_edit
  end
 
