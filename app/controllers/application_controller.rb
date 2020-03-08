4947ca6d8aff4f30365ab4131c3ec6e33e55d197
RYOTA <kobaryo04ysh@icloud.com>
2020-03-08 19:13:04 +0100

    applicationControllerにアカウントの更新を許可するコードを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
 
     
     protected
-    # 入力フォームからアカウント名情報をDBに保存するために追加
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
